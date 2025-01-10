c43872f3142e1eef2263e3a4d5704db946ca0540
Roman Kokorev <kokorev900@gmail.com>
2025-01-10 11:31:10 +0300

    undo packet length change

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * File:   main.c
  * Author: jaybeepee
  *
@@ -10,7 +10,7 @@
 #include <pcap.h>
 
 /*
- * 
+ *
  */
 int main(int argc, char** argv)
 {
@@ -20,7 +20,6 @@ int main(int argc, char** argv)
 	pcap_dumper_t* dumper = NULL;
 	unsigned char* data;
 	unsigned char* new_data;
-	unsigned int new_data_len;
 	struct pcap_pkthdr pkt;
 	char errbuf[PCAP_ERRBUF_SIZE];
 	unsigned int count = 0;
@@ -37,22 +36,21 @@ int main(int argc, char** argv)
 		printf("unable to open file: '%s'\n", infile);
 		goto done;
 	}
-	
+
 	if (DLT_LINUX_SLL != pcap_datalink(handle)) {
 		printf("not an SLL linktype\n");
 		goto done;
 	}
-	
+
 	out_handle = pcap_open_dead(DLT_EN10MB, 65535);
 	if (!out_handle) {
 		printf("unable to open output file: '%s'\n", outfile);
 		goto done;
 	}
 	dumper = pcap_dump_open(out_handle, outfile);
-	
+
 	while ((data=(unsigned char*)pcap_next(handle, &pkt)) != NULL) {
 		new_data = data;
-		new_data_len = pkt.len - 2;
 		int sll_type_hb = data[14];
 		int sll_type_lb = data[15];
 		for (int i=2; i < 14; i++) {
@@ -60,15 +58,14 @@ int main(int argc, char** argv)
 		}
 		new_data[14] = sll_type_hb;
 		new_data[15] = sll_type_lb;
-		pkt.len -= 2;
 		pcap_dump((unsigned char*)dumper, &pkt, &new_data[2]);
 		count++;
 	}
-	
+
 	done:
-	
+
 	printf("%u packets processed\n", count);
-	
+
 	if (handle)
 		pcap_close(handle);
 	if (out_handle)
@@ -76,4 +73,3 @@ int main(int argc, char** argv)
 
 	return(EXIT_SUCCESS);
 }
-
